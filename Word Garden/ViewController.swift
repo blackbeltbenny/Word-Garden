378e2bed61610d5aaafd584e100edcdc0069b0d4
benmeisenzahl <benny.meisenzahl@gmail.com>
2018-09-12 20:50:21 -0400

    Initial Commit

---
+++ Word Garden/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Word Garden
+//
+//  Created by Ben Meisenzahl on 9/12/18.
+//  Copyright © 2018 Ben Meisenzahl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
